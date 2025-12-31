1a4158b9fa5c88450f0c4c52822279c0dd6849f0
Athul <athulrajvv675@gmail.com>
2025-12-31 12:32:22 +0000

    lalla

--- spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/web/VisitResource.java
+++ spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/web/VisitResource.java
@@ -1,82 +1,29 @@
-/*
- * Copyright 2002-2021 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.springframework.samples.petclinic.visits.web;
 
-import java.util.List;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Min;
-
-import io.micrometer.core.annotation.Timed;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.samples.petclinic.visits.model.Visit;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.samples.petclinic.visits.model.VisitRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.test.web.servlet.MockMvc;
 
-/**
- * @author Juergen Hoeller
- * @author Ken Krebs
- * @author Arjen Poutsma
- * @author Michael Isvy
- * @author Maciej Szarlinski
- * @author Ramazan Sakin
- */
-@RestController
-@Timed("petclinic.visit")
-class VisitResource {
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-    private static final Logger log = LoggerFactory.getLogger(VisitResource.class);
+@WebMvcTest(VisitResource.class)
+class VisitResourceTest {
 
-    private final VisitRepository visitRepository;
+    @Autowired
+    private MockMvc mockMvc;
 
-    VisitResource(VisitRepository visitRepository) {
-        this.visitRepository = visitRepository;
-    }
+    @MockBean
+    private VisitRepository visitRepository;
 
-    @PostMapping("owners/*/pets/{petId}/visits")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Visit create(
-        @Valid @RequestBody Visit visit,
-        @PathVariable("petId") @Min(1) int petId) {
-
-        visit.setPetId(petId);
-        log.info("Saving visit {}", visit);
-        return visitRepository.save(visit);
-    }
-
-    @GetMapping("owners/*/pets/{petId}/visits")
-    public List<Visit> read(@PathVariable("petId") @Min(1) int petId) {
-        return visitRepository.findByPetId(petId);
-    }
-
-    @GetMapping("pets/visits")
-    public Visits read(@RequestParam("petId") List<Integer> petIds) {
-        final List<Visit> byPetIdIn = visitRepository.findByPetIdIn(petIds);
-        return new Visits(byPetIdIn);
-    }
-
-    record Visits(
-        List<Visit> items
-    ) {
+    @Test
+    void shouldGetVisits() throws Exception {
+        mockMvc.perform(get("/owners/1/pets/1/visits")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
     }
 }
